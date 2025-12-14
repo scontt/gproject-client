7fb742e195f32c909eefe727500db53477b193e3
scontt <deelog1233223@gmail.com>
2025-12-15 02:06:59 +0700

    base url

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'https://localhost:7272/api',
+  baseURL: '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
